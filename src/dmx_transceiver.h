6b7cc9a866575e3b740f204686f704bce8c876d6
MarianoDel <marianodeleu@yahoo.com.ar>
2016-03-27 19:10:42 -0300

    Prueba eclipse con github

---
+++ src/dmx_transceiver.h
@@ -0,0 +1,27 @@
+/*
+ * dmx_transceiver.h
+ *
+ *  Created on: 09/02/2016
+ *      Author: Mariano
+ */
+
+#ifndef DMX_TRANSCEIVER_H_
+#define DMX_TRANSCEIVER_H_
+
+
+//--- ESTADOS DE TRANSMISION DE PAQUETES DMX512
+#define PCKT_INIT			0
+#define PCKT_END_BREAK		1
+#define PCKT_END_MARK		2
+#define PCKT_TRANSMITING	3
+#define PCKT_END_TX			4
+
+
+#define PCKT_UPDATE			10
+
+//--- FUNCIONES DEL MODULO ---//
+void SendDMXPacket (unsigned char);
+void DMX_Ena(void);
+void DMX_Disa(void);
+
+#endif /* DMX_TRANSCEIVER_H_ */
