268b79a53731fb7824371ddcaeb9dee4e7dffb46
MarianoDel <marianodeleu@yahoo.com.ar>
2016-04-26 10:42:18 -0300

    para presentaciones tecla + wifi local

---
+++ src/HLK_RM04.c
@@ -0,0 +1,8 @@
+/*
+ * HLK_RM04.c
+ *
+ *  Created on: 26/04/2016
+ *      Author: Mariano
+ */
+
+
---
+++ src/HLK_RM04.h
@@ -0,0 +1,13 @@
+/*
+ * HLK_RM04.h
+ *
+ *  Created on: 26/04/2016
+ *      Author: Mariano
+ */
+
+#ifndef HLK_RM04_H_
+#define HLK_RM04_H_
+
+
+
+#endif /* HLK_RM04_H_ */
